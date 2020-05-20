9274f3b08a15183a419922ce15ff215d3087a622
mekasan <borysovk@gmail.com>
2020-05-20 18:51:13 +0300

    added customer service

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerService } from './services/customer.service';
 import { CustomersRouting } from './customers-routing';
 @NgModule({
   declarations: [
@@ -10,6 +11,6 @@ import { CustomersRouting } from './customers-routing';
     CommonModule,
     CustomersRouting,
   ],
-  providers: []
+  providers: [CustomerService]
 })
 export class CustomersModule { }
---
+++ src/app/customers/services/customer.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+
+export interface Orders {
+  id: number;
+  date: Date;
+  sum: number;
+}
+
+export interface Customer {
+  id: number;
+  name: string;
+  status: string;
+  sum: number;
+  orders: Orders[];
+}
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  /**
+   * Get Customers
+   * @return array of Customers
+   */
+  getCustomers(): Customer[] {
+    return [
+      {id: 1, name: 'Ada Clarke', status: 'gold', sum: 12457, orders: [
+          {id: 12, date: new Date( '2020-01-18'), sum: 1967},
+          {id: 25, date: new Date('2020-04-23'), sum: 10490},
+        ]},
+      {id: 2, name: 'Grace Hooper', status: 'regular', sum: 47899, orders: [
+          {id: 55, date: new Date( '2020-03-15'), sum: 22455},
+          {id: 56, date: new Date('2020-04-21'), sum: 25444},
+        ]},
+      {id: 3, name: 'Joan Lovelace', status: 'gold', sum: 5555, orders: [
+          {id: 57, date: new Date( '2020-02-28'), sum: 2777.5},
+          {id: 58, date: new Date('2020-03-13'), sum: 2777.5},
+        ]},
+      {id: 4, name: 'Joan Lovelace', status: 'gold', sum: 5556, orders: [
+          {id: 59, date: new Date( '2020-04-28'), sum: 2778},
+          {id: 60, date: new Date('2020-04-13'), sum: 2778},
+        ]}
+    ];
+  }
+}
